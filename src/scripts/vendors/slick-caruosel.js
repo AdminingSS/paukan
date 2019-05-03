f619ebfdefd825dcd65eebb11b00b1e8b76fe8c0
AdminingSS <admining@mail.ru>
2019-05-03 22:19:26 +0300

    many slider fixes and more

--- src/scripts/vendors/slick-caruosel.js
+++ src/scripts/vendors/slick-caruosel.js
@@ -8,7 +8,29 @@ $(() => {
         const $slider = $('.tm-slider-popular');
         const $body = $('body');
         const $window = $(window);
-        const options = {};
+        const options = {
+            responsive: [
+                {
+                    breakpoint: 960,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        centerMode: true,
+                        centerPadding: '40px'
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        };
         let sliderCreated = false;
 
         sliderInint();
@@ -16,10 +38,10 @@ $(() => {
         $window.on('resize', sliderInint);
 
         function sliderInint() {
-            if (!sliderCreated && $body.width() <= 959) {
+            if (!sliderCreated && $body.width() <= 942) {
                 $slider.slick(options);
                 sliderCreated = true;
-            } else if (sliderCreated && $body.width() > 959) {
+            } else if (sliderCreated && $body.width() > 942) {
                 $slider.slick('unslick');
                 sliderCreated = false;
             }
