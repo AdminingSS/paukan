1c62ecafc42db9aaebdf73b04ed96ef7dcb30093
AdminingSS <admining@mail.ru>
2019-05-05 16:35:35 +0300

    purchases v.2

--- src/scripts/app/mCSB.js
+++ src/scripts/app/mCSB.js
@@ -2,13 +2,22 @@ import 'malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar';
 import 'malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min'
 import 'malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css';
 
+var cx, dx = 0;
+
 $(()=>{
     //scroll
     (() => {
         const $scrollContainer = $('.js-scroll');
+        const $scrollXContainer = $('.js-scroll-x');
 
         $scrollContainer.mCustomScrollbar({
             mouseWheel:{ enable: true }
+        });
+
+        $scrollXContainer.mCustomScrollbar({
+            axis:"x",
+            mouseWheel:{ enable: true },
+            advanced:{ autoExpandHorizontalScroll:true }
         });
     })();
 });
