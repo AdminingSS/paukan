646a1fd11e64a427942a95196cf8682b75cc52a2
AdminingSS <admining@mail.ru>
2019-05-08 16:43:55 +0300

    index fixed

--- src/scripts/vendors/uikit.js
+++ src/scripts/vendors/uikit.js
@@ -5,6 +5,32 @@ import Icons from 'uikit/dist/js/uikit-icons';
 UIkit.use(Icons);
 
 $(() => {
+
+    //select addon
+    (() => {
+        const $selects = $('.tm-select .select-wrapper');
+
+        $selects.each(function () {
+            const $selectWrapper = $(this);
+            const $select = $selectWrapper.find('.select-trigger');
+            const $dropdown = $selectWrapper.find('.select-dropdown');
+            const $input = $selectWrapper.find('input');
+
+            $dropdown.on('click', '.select-option', event, function () {
+                const $target = $(event.target);
+
+                if (!$target.attr('data-value')) return false;
+
+                const optionText = $target.text();
+
+                $select.text(optionText);
+                $input.val($target.attr('data-value'));
+
+                UIkit.dropdown($dropdown).hide();
+            });
+        })
+    })();
+
     //toggler aadon
     (function () {
         const $togglers = $('[uk-toggle]');
